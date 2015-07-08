ca620a23769c1d6186590e64f1699b74d599b7e6
Frank R. Gutacker <spoofer@gmx.net>
2015-07-08 15:19:59 +0200

    Add footer

--- uberall.js
+++ uberall.js
@@ -1,26 +1,15 @@
 uberall = new Mongo.Collection("uberall");
 
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    Template.footer.helpers({
+        startime: function () {
+            return Math.round(new Date().getTime() / 1000 + 24 * 365 * 24 * 60 * 60); // time plus 24 years
+        }
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    
-  });
-}
+    Meteor.startup(function () {
 
+    });
+}
