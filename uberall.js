19b0553cdb859b20e3cea2d597c04bb11815208c
Frank R. Gutacker <spoofer@gmx.net>
2015-07-08 21:32:11 +0200

    Add initial galaxy handling

--- uberall.js
+++ uberall.js
@@ -1,7 +1,20 @@
 uberall = new Mongo.Collection("uberall");
 
+if (uberall.galaxies === undefined) {
+    console.log('No galaxy found');
+}
+;
+
 if (Meteor.isClient) {
+    Accounts.ui.config({
+        forceEmailLowercase: true,
+    });
+    accountsUIBootstrap3.setLanguage('de');
+
     Template.footer.helpers({
+        galaxyCount: function () {
+            return 0; // uberall.galaxies.find().count();
+        },
         startime: function () {
             return Math.round(new Date().getTime() / 1000 + 24 * 365 * 24 * 60 * 60); // time plus 24 years
         }
