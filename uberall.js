f56e201e3a429809ff768ca4d5061150ceaca3f2
Frank R. Gutacker <spoofer@gmx.net>
2015-07-18 10:02:36 +0200

    Add stars

--- uberall.js
+++ uberall.js
@@ -1,5 +1,4 @@
 Galaxies = new Mongo.Collection('Galaxies');
-
 if (Meteor.isClient) {
     Accounts.ui.config({
         forceEmailLowercase: true,
@@ -7,6 +6,27 @@ if (Meteor.isClient) {
     accountsUIBootstrap3.setLanguage('de');
     Accounts.onLogin(function () {
         // noop
+    });
+
+    Template.starmap.onRendered(function () {
+        var size = {
+            height: $('#starmap').height(),
+            width: $('#starmap').width()
+        };
+        var c = document.getElementById("starmap");
+        var ctx = c.getContext("2d");
+        ctx.fillStyle = "#000";
+        ctx.fillRect(0, 0, size.width, size.height);
+        var starColors = ['#fff', '#ccc', '#999', '#666', '#333'];
+        for (var i = 0; i < 5000; i++) {
+            var color = starColors[Math.round(Math.random() * starColors.length)];
+            ctx.fillStyle = color;
+            var pos = {
+                x: Math.round(Math.random() * size.width + 1),
+                y: Math.round(Math.random() * size.height + 1)
+            };
+            ctx.fillRect(pos.x, pos.y, 1, 1);
+        }
     });
 
     Template.footer.helpers({
@@ -17,7 +37,6 @@ if (Meteor.isClient) {
             return Session.get('startime');
         }
     });
-
     setInterval(function () {
         Session.set('startime', Math.round(new Date().getTime() / 1000 + 24 * 365 * 24 * 60 * 60));
     }, 1000);
