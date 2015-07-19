67ceb72a448a3f98aee5522c5a88bbf2c50bb63b
Frank R. Gutacker <spoofer@gmx.net>
2015-07-19 18:14:48 +0200

    Change star calculation

--- uberall.js
+++ uberall.js
@@ -1,4 +1,13 @@
 Galaxies = new Mongo.Collection('Galaxies');
+
+function setPixel(imageData, r, g, b, a) {
+    imageData.data[0] = r;
+    imageData.data[1] = g;
+    imageData.data[2] = b;
+    imageData.data[3] = a;
+    return imageData;
+}
+
 if (Meteor.isClient) {
     Accounts.ui.config({
         forceEmailLowercase: true,
@@ -9,6 +18,8 @@ if (Meteor.isClient) {
     });
 
     Template.starmap.onRendered(function () {
+        $('#starmap').height($('.footer').offset().top - $('#starmap').offset().top - 20);
+        $('#starmap').width($('.navbar').width());
         var size = {
             height: $('#starmap').height(),
             width: $('#starmap').width()
@@ -17,15 +28,17 @@ if (Meteor.isClient) {
         var ctx = c.getContext("2d");
         ctx.fillStyle = "#000";
         ctx.fillRect(0, 0, size.width, size.height);
-        var starColors = ['#fff', '#ccc', '#999', '#666', '#333'];
-        for (var i = 0; i < 5000; i++) {
-            var color = starColors[Math.round(Math.random() * starColors.length)];
-            ctx.fillStyle = color;
+        var img = ctx.createImageData(1, 1);
+        for (var i = 0; i < 2500; i++) {
+            img.data[0] = 1;
+            img.data[1] = 1;
+            img.data[2] = 1;
+            img.data[3] = Math.round(Math.random() * 127 + 1);
             var pos = {
                 x: Math.round(Math.random() * size.width + 1),
                 y: Math.round(Math.random() * size.height + 1)
             };
-            ctx.fillRect(pos.x, pos.y, 1, 1);
+            ctx.putImageData(img, pos.x, pos.y);
         }
     });
 
