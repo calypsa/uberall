1a2549604cc9b1cc936c503c67a3a2bd54a4bdaa
CALYPSA <contact@calypsa.de>
2015-07-20 11:30:16 +0100

    Draw better star map

--- uberall.js
+++ uberall.js
@@ -1,13 +1,5 @@
 Galaxies = new Mongo.Collection('Galaxies');
 
-function setPixel(imageData, r, g, b, a) {
-    imageData.data[0] = r;
-    imageData.data[1] = g;
-    imageData.data[2] = b;
-    imageData.data[3] = a;
-    return imageData;
-}
-
 if (Meteor.isClient) {
     Accounts.ui.config({
         forceEmailLowercase: true,
@@ -18,27 +10,27 @@ if (Meteor.isClient) {
     });
 
     Template.starmap.onRendered(function () {
-        $('#starmap').height($('.footer').offset().top - $('#starmap').offset().top - 20);
-        $('#starmap').width($('.navbar').width());
-        var size = {
-            height: $('#starmap').height(),
-            width: $('#starmap').width()
-        };
+	var size = {
+	    height: Math.round($('.footer').offset().top - $('#starmap').offset().top - 20),
+            width: Math.round($('.navbar').width())
+	}
+	$('#starmap').css('height', size.height);
+	$('#starmap').css('width', size.width);
         var c = document.getElementById("starmap");
-        var ctx = c.getContext("2d");
+	c.height = size.height;
+	c.width = size.width;
+	var ctx = c.getContext("2d");
+	ctx.lineWidth = 1;
         ctx.fillStyle = "#000";
         ctx.fillRect(0, 0, size.width, size.height);
-        var img = ctx.createImageData(1, 1);
-        for (var i = 0; i < 2500; i++) {
-            img.data[0] = 1;
-            img.data[1] = 1;
-            img.data[2] = 1;
-            img.data[3] = Math.round(Math.random() * 127 + 1);
+	ctx.fillStyle = "#fff";
+        for (var i = 0; i < 1000; i++) {
             var pos = {
                 x: Math.round(Math.random() * size.width + 1),
                 y: Math.round(Math.random() * size.height + 1)
             };
-            ctx.putImageData(img, pos.x, pos.y);
+	    ctx.globalAlpha = Math.random();
+            ctx.fillRect(pos.x, pos.y, 1, 1);
         }
     });
 
